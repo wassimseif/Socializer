dc9fa7fbd4646de397a277806c15db518c341506
Wassim  Seifeddine <wassim_seifeddine95@live.com>
2017-09-05 09:56:54 +0300

    Initial Commit

---
+++ Socializer/Socializer.h
@@ -0,0 +1,19 @@
+//
+//  Socializer.h
+//  Socializer
+//
+//  Created by Wassim on 9/5/17.
+//  Copyright © 2017 wassimseif. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Socializer.
+FOUNDATION_EXPORT double SocializerVersionNumber;
+
+//! Project version string for Socializer.
+FOUNDATION_EXPORT const unsigned char SocializerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Socializer/PublicHeader.h>
+
+
